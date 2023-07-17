71b093cc6c62f459f7f5334236d237ebb91493cf
FanoDre <134488457+FanoDre@users.noreply.github.com>
2023-07-17 15:42:22 +0700

    Update arrRec.py

--- arrRec.py
+++ arrRec.py
@@ -20,22 +20,44 @@ config = {
 # GUI
 nimLabel = Label(root, text="NIM", font=('Arial', 15))
 namaLabel = Label(root, text="Nama", font=('Arial', 15))
-nimLabel.grid(row=1, column=0, padx=10)
-namaLabel.grid(row=2, column=0, padx=10)
-
-nimEntry = Entry(root, font=('Arial', 15), width=20)
-namaEntry = Entry(root, font=('Arial', 15), width=20)
-nimEntry.grid(row=1, column=1)
-namaEntry.grid(row=2, column=1)
+nimLabel.grid(row=1, column=0, padx=10, pady=10)
+namaLabel.grid(row=2, column=0, padx=10, pady=10)
 
 nilaiDisipLabel = Label(root, text="Nilai Disiplin (25%)", font=('Arial', 15))
 nilaiEtikaLabel = Label(root, text="Nilai Etika (25%)", font=('Arial', 15))
-nilaiDisipLabel.grid(row=1, column=2)
-nilaiEtikaLabel.grid(row=2, column=2)
+nilaiDisipLabel.grid(row=3, column=0, padx=10, pady=10)
+nilaiEtikaLabel.grid(row=4, column=0, padx=10, pady=10)
 
-nilaiDisipEntry = Entry(root, font=('Arial', 15), width=20)
-nilaiEtikaEntry = Entry(root, font=('Arial', 15), width=20)
-nilaiDisipEntry.grid(row=1, column=3)
-nilaiEtikaEntry.grid(row=2, column=3)
+nilaiEtosLabel = Label(root, text="Nilai Etos (25%)", font=('Arial', 15))
+nilaiInovasiLabel = Label(root, text="Nilai Inovasi (25%)", font=('Arial', 15))
+nilaiEtosLabel.grid(row=5, column=0, padx=10, pady=10)
+nilaiInovasiLabel.grid(row=6, column=0, padx=10, pady=10)
+
+nimEntry = Entry(root, font=('Arial', 15), width=15)
+namaEntry = Entry(root, font=('Arial', 15), width=15)
+nimEntry.grid(row=1, column=1)
+namaEntry.grid(row=2, column=1)
+
+nilaiDisipEntry = Entry(root, font=('Arial', 15), width=15)
+nilaiEtikaEntry = Entry(root, font=('Arial', 15), width=15)
+nilaiDisipEntry.grid(row=3, column=1)
+nilaiEtikaEntry.grid(row=4, column=1)
+
+nilaiEtosEntry = Entry(root, font=('Arial', 15), width=15)
+nilaiInovasiEntry = Entry(root, font=('Arial', 15), width=15)
+nilaiEtosEntry.grid(row=5, column=1)
+nilaiInovasiEntry.grid(row=6, column=1)
+
+buttonSimpan = Button(root, text="Simpan", font=('Arial', 15), width=10, height=2)
+buttonSimpan.grid(row=7, column=0, padx=10, pady=10)
+
+buttonHapus = Button(root, text="Hapus", font=('Arial', 15), width=10, height=2)
+buttonHapus.grid(row=7, column=1, padx=10, pady=10)
+
+buttonEdit = Button(root, text="Edit", font=('Arial', 15), width=10, height=2)
+buttonEdit.grid(row=7, column=2, padx=10, pady=10)
+
+buttonTutup = Button(root, text="Tutup", font=('Arial', 15), width=10, height=2)
+buttonTutup.grid(row=7, column=3, padx=10, pady=10)
 
 root.mainloop()
